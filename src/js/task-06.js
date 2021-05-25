e9ef48327b84699a4d927580665ba77a96e5abbf
Boiko Roman <boykoroman94@gmail.com>
2021-05-25 20:48:42 +0300

    исправляет проверку условия валидности

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -12,12 +12,12 @@
 const inputEl = document.querySelector('#validation-input');
 
 const onFocusLost = event => {
-    if (event.currentTarget.value.length < inputEl.dataset.length) {
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
-    } else {
+    if (event.currentTarget.value.length === parseInt(inputEl.dataset.length)) {
         inputEl.classList.add('valid');
         inputEl.classList.remove('invalid');
+    } else {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
     }
 };
 
